f8abdf6703595bb21c4ad07efeff75e73eb92ca9
MrKessie <koranteng.kwesi@zeushub.cloud>
2026-05-13 09:19:28 +0000

    Initial Commit

---
+++ src/main/java/com/rhc/RuralHealthConnectApplication.java
@@ -0,0 +1,13 @@
+package com.rhc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralHealthConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralHealthConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhc/RuralHealthConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralHealthConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
